5a96e2bdaacdb6bcb4901d90ad7a7a09ad514644
gregsmith446 <gregsmith446@gmail.com>
2017-11-28 16:51:03 +0000

    choosing option now displays data

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var carRental = {  
+var carRental = { //holds all the info
     cars: [
     {
         type: "Economy", 
@@ -13,24 +13,42 @@ var carRental = {
     ]
 };
 
+var renterData = { //a place for the data to go when submitted
+        name:[],
+        car:[],
+}
+
 //this adds the 2 car types - economy and SUV to the dropdown - *WORKS*
 for (var i = 0; i < carRental.cars.length; i++) {    
-    var options = document.createElement("OPTION");
-    options.text = carRental.cars[i].type;
-    document.getElementById("dropdown").appendChild(options);
-    options.setAttribute("value", i); //gives the options economy and SUV the value of 0 and 1; value is for us not for the user
-    options.setAttribute("id", "car" + i); //gives the economy and SUV options the car0 and car1 IDs
-    //would like to add a hidden option to the dropdown but do not know how
+    var option = document.createElement("OPTION");
+    option.text = carRental.cars[i].type;
+    document.getElementById("dropdown").appendChild(option);
+    option.setAttribute("value", i); //gives the options economy and SUV the value of 0 and 1; value is for us not for the user
+    option.setAttribute("id", "car" + i); //gives the economy and SUV options the car0 and car1 IDs
+    console.log(option.value);
 }
-//this nameless function is assigned to the onchange event handler 
-//should show the car price and amount available when selected DOES NOT WORK
-document.getElementById("car0").onchange = function(){
-        document.getElementById("available").innerHTML = carRental.cars[0].available;
-        document.getElementById("price").innerHTML = carRental.cars[0].price;
-    };
-    
-document.getElementById("car1").onchange = function(){
-        document.getElementById("available").innerHTML = carRental.cars[1].available;
-        document.getElementById("price").innerHTML = carRental.cars[1].price;
-        
-    };
+
+document.getElementById("dropdown").onchange = function(){ //this nameless function is assigned to the onchange event handler 
+    var select = document.getElementById("dropdown");
+    // console.log(select.value);
+    document.getElementById("price").innerHTML = carRental.cars[select.value].price;
+    document.getElementById("available").innerHTML = carRental.cars[select.value].available;
+    //this connects the car price+avail data to the HTML *WORKS*
+}
+
+function formValidation(){ //form validation - prevent form submission until correct.
+	       // And Form Validation using an else if statement - Prevents form submission is form is incomplete - triggers an alert message
+	       var renterName = document.getElementById("renterName")
+	       if(option == "none"){
+	           alert("Please choose a car type");
+	       }
+	       else if(renterName == ""){
+	           alert("Please enter your first and last name");
+	       } else {
+	           document.getElementById('displayInfo').innerHTML = "Your reservation was successful!"
+	       }
+	       return false;
+	   }
+
+// add a section here that will deduct a car from the amount when form is submitted.
+// cars.econcount--;
